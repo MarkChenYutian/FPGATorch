458c1ff61f8e949553d258341f8bf72e3b3235f1
Felicia Liu <96500559+felicialyx@users.noreply.github.com>
2024-01-15 13:45:47 -0500

    Systolic array prototype

---
+++ RTL/SystolicArray.sv
@@ -0,0 +1,47 @@
+`default_nettype none
+
+// Perform matrix multiplication using systolic array algorithm
+module SystolicArray #(parameter N = 8)
+  (input logic reset, clock,
+   input logic [N-1:0][31:0] A, B,
+   output logic [N-1:0][N-1:0][31:0] Out);
+  
+  // Input ShiftReg for each processor
+  // A: row shift  B: col shift
+  logic [N-1:0][N-1:0][31:0] A_SR, B_SR;
+
+  genvar r, c;
+  generate
+    for (r = 0; r < N; r++)
+      for (c = 0; c < N; c++) begin: SAProcessor
+        always_ff @(posedge clock) begin
+          // Handle reset
+          if (reset) begin
+            A_SR[r][c] <= 'b0;
+            B_SR[r][c] <= 'b0;
+            Out[r][c] <= 'b0;
+          end
+          else begin
+            // Mult + Accum
+            Out[r][c] <= Out[r][c] + A_SR[r][c]*B_SR[r][c];
+            // Handle ShiftReg
+            // A
+            if (c == 0) begin
+              A_SR[r][c] <= A[r];
+            end
+            else begin
+              A_SR[r][c] <= A_SR[r][c-1];
+            end
+            // B
+            if (r == 0) begin
+              B_SR[r][c] <= B[c];
+            end
+            else begin
+              B_SR[r][c] <= B_SR[r-1][c];
+            end
+          end
+        end
+    end: SAProcessor
+  endgenerate
+
+endmodule: SystolicArray
---
+++ RTL/SystolicArray_TB.sv
@@ -0,0 +1,54 @@
+`default_nettype none
+
+module SystolicArray_TB;
+  logic clock, reset;
+  logic [1:0][31:0] A, B;
+  logic [1:0][1:0][31:0] Out;
+
+  SystolicArray #(2) DUT(.*);
+
+  logic [2:0][1:0][31:0] A_input, B_input;
+  always_comb begin
+    A_input[0] = {0, 32'd3};
+    A_input[1] = {32'd4, 32'd6};
+    A_input[2] = {32'd5, 0};
+    // 6 3
+    // 5 4
+
+    B_input[0] = {0, 32'd2};
+    B_input[1] = {32'd1, 32'd10};
+    B_input[2] = {32'd8, 0};
+    // 10 8
+    // 2 1
+
+  end
+
+  initial begin
+    clock = 1'b0;
+    forever #5 clock = ~clock;
+  end
+  
+  initial begin
+    $monitor($time,, ", %d %d \\ %d %d", Out[0][0], Out[0][1], Out[1][0], Out[1][1]);
+    reset <= 1;
+    @(posedge clock)
+    reset <= 0;
+    A <= A_input[0];
+    B <= B_input[0];
+    @(posedge clock)
+    A <= A_input[1];
+    B <= B_input[1];
+    @(posedge clock)
+    A <= A_input[2];
+    B <= B_input[2];
+    @(posedge clock)
+    A <= 64'b0;
+    B <= 64'b0;
+    @(posedge clock)
+    @(posedge clock)
+    @(posedge clock)
+    #30
+    $finish;
+  end
+
+endmodule: SystolicArray_TB
