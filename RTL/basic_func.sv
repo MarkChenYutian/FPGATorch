4873f60b3b0fa50191b213bfe262453ce0f57496
mzy-cmu <zhiyingm@andrew.cmu.edu>
2024-01-18 16:49:27 -0500

    add again

--- RTL/basic_func.sv
+++ RTL/basic_func.sv
@@ -159,39 +159,3 @@ module MatAdd // read -> add -> save
                     .zero());
 
 endmodule : MatAdd
-
-
-module Register
-  #(parameter WIDTH=8)
-  (input  logic [WIDTH-1:0] D,
-   input  logic             en, clear, clock,
-   output logic [WIDTH-1:0] Q);
-   
-  always_ff @(posedge clock)
-    if (en)
-      Q <= D;
-    else if (clear)
-      Q <= '0;
-      
-endmodule : Register
-
-
-module Counter
-  #(parameter WIDTH=8,
-              STEP=1)
-  (input  logic [WIDTH-1:0] D,
-   input  logic             en, clear, load, clock, up,
-   output logic [WIDTH-1:0] Q);
-   
-  always_ff @(posedge clock)
-    if (clear)
-      Q <= {WIDTH {1'b0}};
-    else if (load)
-      Q <= D;
-    else if (en)
-      if (up)
-        Q <= Q + STEP;
-      else
-        Q <= Q - STEP;
-        
-endmodule : Counter
---
+++ RTL/library.sv
@@ -0,0 +1,34 @@
+module Register
+  #(parameter WIDTH=8)
+  (input  logic [WIDTH-1:0] D,
+   input  logic             en, clear, clock,
+   output logic [WIDTH-1:0] Q);
+   
+  always_ff @(posedge clock)
+    if (en)
+      Q <= D;
+    else if (clear)
+      Q <= '0;
+      
+endmodule : Register
+
+
+module Counter
+  #(parameter WIDTH=8,
+              STEP=1)
+  (input  logic [WIDTH-1:0] D,
+   input  logic             en, clear, load, clock, up,
+   output logic [WIDTH-1:0] Q);
+   
+  always_ff @(posedge clock)
+    if (clear)
+      Q <= {WIDTH {1'b0}};
+    else if (load)
+      Q <= D;
+    else if (en)
+      if (up)
+        Q <= Q + STEP;
+      else
+        Q <= Q - STEP;
+        
+endmodule : Counter
