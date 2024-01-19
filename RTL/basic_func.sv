a0470282ef164eff9a1c786760e7b2535294f50d
mzy-cmu <zhiyingm@andrew.cmu.edu>
2024-01-18 23:46:21 -0500

    compile memory access fsm, kinda works from pure waveform observation

--- RTL/basic_func.sv
+++ RTL/basic_func.sv
@@ -1,11 +1,11 @@
 `default_nettype none
 
 `define SIZE_MAX 64
-`define OP_NUM 11
 
-`define OP_ADDR 999
-`define DATA_ADDR 999
-`define RES_ADDR 999
+`define OP_ADDR 0
+`define SCALAR_ADDR 10
+`define DATA_ADDR 100
+`define RES_ADDR 200
 
 `define ADDR_WIDTH 12
 `define DATA_WIDTH 32
@@ -14,28 +14,26 @@
 
 `define READ_BW 4
 
-`define SINGLE_MAT_ADD 8
-`define SINGLE_MUL 8
-`define SINGLE_DIV 8
-`define SINGLE_ADD 8
-`define SINGLE_INV 8
-`define SINGLE_MAT_MUL 8
+`define SINGLE_ACCESS 8
 
+`define CYCLE_WIDTH 3
+`define CYCLE_MAT_ADD 5
+`define CYCLE_MUL 5
+`define CYCLE_DIV 5
+`define CYCLE_ADD 5
+`define CYCLE_INV 5
+`define CYCLE_MAT_MUL 5
 
-package OPpkg
-
-  typedef enum logic [$clog(`OP_NUM)-1:0]
-    { NONE = 0,
-      MAT_ADD = 1,
-      MAT_SCAL_MUL = 2,
-      MAT_SCAL_DIV = 3,
-      MAT_SCAL_ADD = 4,
-      MAT_SCAL_INV = 5,
-      MAT_MUL = 6,
-      MAT_TRAS = 7,
-      REDUCE_SUM = 8} op_t;
-
-endpackage : OPpkg
+typedef enum logic [`OP_WIDTH-1:0]
+  { NONE = 0,
+    MAT_ADD = 1,
+    MAT_SCAL_MUL = 2,
+    MAT_SCAL_DIV = 3,
+    MAT_SCAL_ADD = 4,
+    MAT_SCAL_INV = 5,
+    MAT_MUL = 6,
+    MAT_TRAS = 7,
+    REDUCE_SUM = 8} op_t;
 
 
 module ALU
@@ -45,51 +43,68 @@ module ALU
 
   // case operation -> inputs
 
-  MatMem #(8) mem (.clock, .reset, .addr(), .size0(), .size1(), .size2(), .mat(A));
 
 endmodule : ALU
 
 
 module FSM
   (input logic clock, reset,
-   input logic [$clog(`OP_NUM)-1:0] op_code,
-   input logic [`DIM_WIDTH-1:0] dim1, dim2,
+   input logic [`OP_WIDTH-1:0] op_code,
+   input logic [`DIM_WIDTH-1:0] dimA1, dimA2,
    output logic [`ADDR_WIDTH-1:0] mem_addr,
    output logic [`DATA_WIDTH-1:0] mem_data,
-   output logic read, write, read_count, write_count);
+   output logic read, write);
 
-  enum logic {WAIT, READ, COMPUTE, WRITE} state, nextState;
+  enum logic [2:0] {WAIT, READ, READ_B, COMPUTE, WRITE} state, nextState;
 
-  logic [`ADDR_WIDTH-1:0] read_ptr, read_single_ptr, write_ptr, write_single_ptr;
+  logic [`ADDR_WIDTH-1:0] read_ptr, write_ptr, block_ptr;
+  logic [`DIM_WIDTH-1:0] dimA_total;
+  logic [`CYCLE_WIDTH-1:0] op_count, op_cycle;
+  logic read_next, write_next, next_block, op_compute;
   
-  Counter #(`ADDR_WIDTH, 1) readAddr (.D(`DATA_ADDR), .en(next), .clear(op_code == NONE), .load(reset), .clock, .Q(read_ptr));
-  Counter #(`ADDR_WIDTH, 1) readSingleAddr (.D(), .en(read_single_next), .clear(read_next), .load(), .clock, .Q(read_single_ptr));
+  Counter #(`ADDR_WIDTH, 1) read_count (.D(0), .en(read_next), .clear(reset), .load(next_block), .clock, .up(1'b1), .Q(read_ptr));
+  Counter #(`ADDR_WIDTH, 1) write_count (.D(0), .en(write_next), .clear(reset), .load(next_block), .clock, .up(1'b1), .Q(write_ptr));
   
-  Counter #(`ADDR_WIDTH, 1) writeAddr (.D(`RES_ADDR), .en(write_next), .clear(op_code == NONE), .load(reset), .clock, .Q(write_ptr));
-  Counter #(`ADDR_WIDTH, 1) writeSingleAddr (.D(), .en(write_single_next), .clear(write_next), .load(), .clock, .Q(write_single_ptr));
+  Counter #(`ADDR_WIDTH, `SINGLE_ACCESS) block_count (.D(0), .en(next_block), .clear(reset), .load(op_code == NONE), .clock, .up(1'b1), .Q(block_ptr));
 
-  Counter #(`ADDR_WIDTH, 1) compute (.D(), .en(read_next), .clear(), .load(reset), .clock, .Q(read_ptr));
+  Counter #(`CYCLE_WIDTH, 1) compute (.D(0), .en(op_compute), .clear(reset), .load(next_block), .clock, .up(1'b1), .Q(op_count));
+
+  assign dimA_total = dimA1 * dimA2;
 
   always_comb begin
     case (op_code)
-      NONE: read_cycle = 0;
-      MAT_ADD: read_cycle = `SINGLE_MAT_ADD;
-      MAT_SCAL_MUL: read_cycle = `SINGLE_MUL;
-      MAT_SCAL_DIV: read_cycle = `SINGLE_DIV;
-      MAT_SCAL_ADD: read_cycle = `SINGLE_ADD;
-      MAT_SCAL_INV: read_cycle = `SINGLE_INV;
-      MAT_MUL: read_cycle = `SINGLE_MAT_MUL;
-      MAT_TRAS: read_cycle = 0;
-      REDUCE_SUM: read_cycle = 0;
-      default: read_cycle = 0;
+      MAT_ADD: op_cycle = `CYCLE_MAT_ADD;
+      MAT_SCAL_MUL: op_cycle = `CYCLE_MUL;
+      MAT_SCAL_DIV: op_cycle = `CYCLE_DIV;
+      MAT_SCAL_ADD: op_cycle = `CYCLE_ADD;
+      MAT_SCAL_INV: op_cycle = `CYCLE_INV;
+      MAT_MUL: op_cycle = `CYCLE_MAT_MUL;
+      default: op_cycle = 0;
     endcase
   end
 
   always_comb
     case (state)
       WAIT: nextState = (op_code == NONE) ? WAIT : READ;
-      READ: nextState = (read_single_ptr < read_cycle) ? COMPUTE : READ;
-      COMPUTE: nextState = done ? WAIT : COMPUTE;
+      READ: begin
+        if (read_ptr < `SINGLE_ACCESS)
+          if (op_code == MAT_ADD)
+            nextState = READ_B;
+          else
+            nextState = READ;
+        else
+          nextState = COMPUTE;
+      end
+      READ_B: nextState = READ;
+      COMPUTE: nextState = (op_count < op_cycle) ? COMPUTE : WRITE;
+      WRITE: begin
+        if (write_ptr < `SINGLE_ACCESS)
+          nextState = WRITE;
+        else if (block_ptr < dimA_total)
+            nextState = READ;
+        else
+          nextState = WAIT;
+      end
     endcase
 
   always_comb begin
@@ -97,24 +112,46 @@ module FSM
     mem_data = 0;
     read = 1'b0;
     write = 1'b0;
+    read_next = 1'b0;
+    write_next = 1'b0;
+    next_block = 1'b0;
+    op_compute = 1'b0;
     case (state)
       WAIT: begin
         if (op_code == NONE) begin
           read = 1'b1;
-          mem_addr = OP_ADDR;
+          mem_addr = `OP_ADDR;
         end else begin
           read = 1'b1;
-          mem_addr = DATA_ADDR;
+          mem_addr = `SCALAR_ADDR;
         end
       end
       READ: begin
-        if (MAT_ADDR < dim1 * dim2) begin
+        if (read_ptr < `SINGLE_ACCESS) begin
           read = 1'b1;
-          mem_addr = `DATA_ADDR + read_ptr;
+          mem_addr = `DATA_ADDR + block_ptr + read_ptr;
+          if (op_code != MAT_ADD)
+            read_next = 1'b1;
         end else begin
+          op_compute = 1'b1;
+        end
+      end
+      READ_B: begin
+        read = 1'b1;
+        mem_addr = `DATA_ADDR + block_ptr + read_ptr + dimA_total;
+        read_next = 1'b1;
+      end
+      COMPUTE: op_compute = (op_count < op_cycle);
+      WRITE: begin
+        if (write_ptr < `SINGLE_ACCESS) begin
           write = 1'b1;
-          mem_data = 0;
+          mem_addr = `RES_ADDR + write_ptr;
+          write_next = 1'b1;
+        end else begin
+          next_block = 1'b1;
+          write = 1'b1;
           mem_addr = `OP_ADDR;
+          mem_data = 0;
         end
       end
     endcase
@@ -129,6 +166,40 @@ module FSM
 endmodule : FSM
 
 
+module FSM_test();
+
+  logic clock, reset;
+  logic [`OP_WIDTH-1:0] op_code;
+  logic [`DIM_WIDTH-1:0] dimA1, dimA2;
+  logic [`ADDR_WIDTH-1:0] mem_addr;
+  logic [`DATA_WIDTH-1:0] mem_data;
+  logic read, write;
+
+  FSM dut (.*);
+
+  initial begin
+    clock = 1'b0;
+    forever #5 clock = ~clock;
+  end
+
+  initial begin
+    reset = 1'b0;
+    reset <= 1'b1;
+
+    @(posedge clock);
+    reset <= 1'b0;
+    op_code <= MAT_ADD;
+    dimA1 <= 4;
+    dimA2 <= 2;
+
+    #1000000;
+
+    $finish;
+  end
+
+endmodule : FSM_test
+
+/*
 module MatMem
   (input logic clock, reset);
   
@@ -141,13 +212,13 @@ module MatMem
   Counter #(8, 1) count0(.D(), .en(), .clear(), .load(), .clock, .Q());
 
 endmodule : MatMem
-
-
+*/
+/*
 module MatAdd // read -> add -> save
  #(parameter SIZE_SINGLE = 8) // depend on #adders
   (input logic clock, reset,
-   input logic [W-1:0] dataA, dataB,
-   output logic [W-1:0] dataC);
+   input logic [`DATA_WIDTH-1:0] dataA, dataB,
+   output logic [`DATA_WIDTH-1:0] dataC);
 
   add_cycle_7_area (.clock,
                     .dataa(dataA),
@@ -159,3 +230,4 @@ module MatAdd // read -> add -> save
                     .zero());
 
 endmodule : MatAdd
+*/
