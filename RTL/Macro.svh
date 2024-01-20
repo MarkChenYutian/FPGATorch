21808cee8916af42d3819602b325fd1f73d32608
Felicia Liu <96500559+felicialyx@users.noreply.github.com>
2024-01-20 17:49:34 -0500

    Create Macro.svh

---
+++ RTL/Macro.svh
@@ -0,0 +1,20 @@
+`define SIZE_MAX 64
+`define OP_NUM 11
+
+`define OP_ADDR 999
+`define DATA_ADDR 999
+`define RES_ADDR 999
+
+`define ADDR_WIDTH 12
+`define DATA_WIDTH 32
+`define OP_WIDTH 4
+`define DIM_WIDTH 6
+
+`define READ_BW 4
+
+`define SINGLE_MAT_ADD 8
+`define SINGLE_MUL 8
+`define SINGLE_DIV 8
+`define SINGLE_ADD 8
+`define SINGLE_INV 8
+`define SINGLE_MAT_MUL 8
