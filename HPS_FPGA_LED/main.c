3471174c58ba6371fa5ee25e56e52621379423a1
A1020A <andrew102000l@gmail.com>
2024-01-21 02:55:42 -0500

    main.c update 3

--- HPS_FPGA_LED/main.c
+++ HPS_FPGA_LED/main.c
@@ -26,6 +26,7 @@ refer to user manual chapter 7 for details about the demo
 int main() {
 	void *instruction_base;
 	void *fpga_mem_base;
+	int fd;
 	int loop_count;
 	int led_direction;
 	int led_mask;
@@ -82,30 +83,30 @@ int main() {
 		printf("%f\n", mat_res[i]);
 	}
 	
-	// loop_count = 0;
-	// led_mask = 0x01;
-	// led_direction = 0; // 0: left to right direction
-	// while( loop_count < 5 ) {
+	loop_count = 0;
+	led_mask = 0x01;
+	led_direction = 0; // 0: left to right direction
+	while( loop_count < 5 ) {
 		
-	// 	// control led
-	// 	*(uint32_t *)h2p_lw_led_addr = ~led_mask; 
+		// control led
+		*(uint32_t *)h2p_lw_led_addr = ~led_mask; 
 
-	// 	// wait 100ms
-	// 	usleep( 100*1000 );
+		// wait 100ms
+		usleep( 100*1000 );
 		
-	// 	// update led mask
-	// 	if (led_direction == 0){
-	// 		led_mask <<= 1;
-	// 		if (led_mask == (0x01 << (LED_PIO_DATA_WIDTH-1)))
-	// 			 led_direction = 1;
-	// 	}else{
-	// 		led_mask >>= 1;
-	// 		if (led_mask == 0x01){ 
-	// 			led_direction = 0;
-	// 			loop_count++;
-	// 		}
-	// 	}
-	// } // while
+		// update led mask
+		if (led_direction == 0){
+			led_mask <<= 1;
+			if (led_mask == (0x01 << (LED_PIO_DATA_WIDTH-1)))
+				 led_direction = 1;
+		}else{
+			led_mask >>= 1;
+			if (led_mask == 0x01){ 
+				led_direction = 0;
+				loop_count++;
+			}
+		}
+	} // while
 	
 	// clean up our memory mapping and exit
 	
